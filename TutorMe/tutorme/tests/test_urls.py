5fb1cd27b8ec90c1c38ee82869f39b0aa0416250
Panayiotis Papapolydorou <papapolydoroup@gmail.com>
2020-03-23 18:38:29 +0000

    finish testing

--- TutorMe/tutorme/tests/test_urls.py
+++ TutorMe/tutorme/tests/test_urls.py
@@ -1,51 +1,29 @@
-# from django.test import SimpleTestCase, Client
-# from django.urls import reverse, resolve
-# from tutorme.views import index, about, register_student, register_teacher, user_login, user_logout, search, dashboard
-
-# client = Client()
+from django.test import TestCase
+from django.urls import reverse
 
 
-# class TestUrls(SimpleTestCase):
-#
-#     def test_index_url_resolves(self):
-#         response = client.get(reverse('create'))
-#         self.assertEqual(response.status_code, 200)
-        # url = reverse('index')
-        # print(resolve(url))
-        # self.assertEquals(resolve(url).func, index)
+class UrlTest(TestCase):
+    def test_about_url(self):
+        response = self.client.get('/tutorme/about/')
+        self.assertEqual(response.status_code, 200)
 
-    # def test_about_url_resolves(self):
-    #     url = reverse('about')
-    #     print(resolve(url))
-    #     self.assertEquals(resolve(url).func, about)
-    #
-    # def test_register_student_url_resolves(self):
-    #     url = reverse('register_student')
-    #     print(resolve(url))
-    #     self.assertEquals(resolve(url).func, register_student)
-    #
-    # def test_register_teacher_url_resolves(self):
-    #     url = reverse('register_teacher')
-    #     print(resolve(url))
-    #     self.assertEquals(resolve(url).func, register_teacher)
-    #
-    # def test_login_url_resolves(self):
-    #     url = reverse('login')
-    #     print(resolve(url))
-    #     self.assertEquals(resolve(url).func, user_login)
-    #
-    # def test_logout_url_resolves(self):
-    #     url = reverse('logout')
-    #     print(resolve(url))
-    #     self.assertEquals(resolve(url).func, user_logout)
-    #
-    # def test_search_url_resolves(self):
-    #     url = reverse('search')
-    #     print(resolve(url))
-    #     self.assertEquals(resolve(url).func, search)
-    #
-    # def test_dashboard_url_resolves(self):
-    #     url = reverse('dashboard')
-    #     print(resolve(url))
-    #     self.assertEquals(resolve(url).func, dashboard)
+    def test_about_url_reverse(self):
+        response = self.client.get(reverse('tutorme:about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_url(self):
+        response = self.client.get('/tutorme/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_url_reverse(self):
+        response = self.client.get(reverse('tutorme:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_url(self):
+        response = self.client.get('/tutorme/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_url_reverse(self):
+        response = self.client.get(reverse('tutorme:login'))
+        self.assertEqual(response.status_code, 200)
 
--- TutorMe/tutorme/tests/test_views.py
+++ TutorMe/tutorme/tests/test_views.py
@@ -1,13 +1,33 @@
-from django.test import TestCase, Client
+from tutorme.models import User,Teacher
 from django.urls import reverse
+from django.test.client import Client
+from django.test import TestCase
 
 
+class ViewsTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user('panayiotis@tutorme.com', 'password')
+        Teacher.objects.create(user=self.user, first_name='Panayiotis')
 
-# class TestViews(TestCase):
-#     def test_index_view_GET(self):
-#         self.client = Client()
-#         self.index = reverse('index')
-#
-#         response = self.client.get("index")
-#         self.assertEqual(response.status_code, 200)
-#         self.assertTemplateUsed(response, 'tutorme/index.html')
+    def test_about(self):
+        self.client.login(email='panayiotis@tutorme.com', password='password')
+        response = self.client.get(reverse('tutorme:about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_dashboard(self):
+        self.client.login(email='panayiotis@tutorme.com', password='password')
+        response = self.client.get(reverse('tutorme:dashboard'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index(self):
+        self.client.login(email='panayiotis@tutorme.com', password='password')
+        response = self.client.get(reverse('tutorme:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_search(self):
+        self.client.login(email='panayiotis@tutorme.com', password='password')
+        response = self.client.get(reverse('tutorme:search'))
+        self.assertEqual(response.status_code, 200)
+
+
